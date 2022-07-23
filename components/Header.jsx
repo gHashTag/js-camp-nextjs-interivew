88bd0aad806fa11a730bcc8668e6bf031001fe59
Dmitriy Vasilyev <dmitrii.v@railsmuffin.com>
2022-07-23 14:22:13 -0500

    added react native

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import Link from 'next/link'
+import { Text } from 'react-native'
 
 export default function Header({ name, title, date, url }) {
   return (
     <header>
       <div>
-        <a href={url}>
-          <span>{name}</span>
-        </a>{' '}
+       
+          <Text style={{ color: 'gold'}}>Hello react native web</Text>
+
         —{' '}
         <Link href="/1">
-          <a>{title}</a>
+          <a>{title}fsf</a>
         </Link>
+        
       </div>
       <time>{date}</time>
     </header>
